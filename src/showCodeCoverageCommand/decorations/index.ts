8ab19d5c503be07192a9623474827c80078c7235
David Milanese <milanese.david@gmail.com>
2020-12-13 17:19:18 +1030

    move common code into index file

--- src/showCodeCoverageCommand/decorations/index.ts
+++ src/showCodeCoverageCommand/decorations/index.ts
@@ -1,11 +1,13 @@
 import {
   DecorationOptions,
+  TextDocument,
   TextEditor,
   TextEditorDecorationType,
 } from 'vscode';
 
 import Resultset from './codeCoverageResults';
-import Decorators from './decorators'
+import Decorators from './decorators';
+import { EditorResults } from '../../types';
 
 type Decoration = {
   editor: TextEditor;
@@ -15,9 +17,28 @@ type Decoration = {
 
 const resultset = Resultset();
 
+const fileNames = (editors: TextEditor[]) =>
+  editors
+    .map((textEditor: TextEditor) => textEditor.document)
+    .map((document: TextDocument) => document.fileName);
+
+const resultsSetsForEditors = (editors: TextEditor[]) =>
+  Object.values(resultset)
+    .map(({ coverage }) => Object.entries(coverage))
+    .map(([[fileName, coverage]]) => ({ fileName, coverage }))
+    .filter(({ fileName }) => fileNames(editors).includes(fileName));
+
+const codeCoverageSets = (editors: TextEditor[]): EditorResults[] =>
+  resultsSetsForEditors(editors).map(({ fileName, coverage }) => ({
+    editor: editors.find(
+      (editor) => editor.document.fileName === fileName
+    ) as TextEditor,
+    coverage,
+  }));
+
 export default (editors: TextEditor[]): Decoration[] =>
   Decorators.flatMap(({ decorations, decorationType }) =>
-    decorations(resultset, editors).map(
+    decorations(codeCoverageSets(editors)).map(
       ({ editor, decorations: decorationOptions }) => ({
         editor,
         decorationType,
